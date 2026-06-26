cf0d09694a57368b9281845a46675195c9f1b627
Giffin-MH-Osborne <giffin.mh.osborne@gmail.com>
2026-06-26 17:36:36 -0400

    Added class definition for File and FileSystem

---
+++ File.h
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include "Node.h"
+
+class File : public Node {
+    std::string content;
+
+public:
+    File(const std::string& name, Node* parent) : Node(name, parent) {}
+
+    NodeType getType() const override {return NodeType::File;}
+
+    void write(const std::string& data) {content = data;}
+    const std::string& read() const {return content;}
+};
---
+++ FileSystem.h
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include "Directory.h"
+#include "File.h"
+
+class FileSystem {
+    Directory* root;
+    Directory* current;
+public:
+    FileSystem(){
+        root = new Directory("/", nullptr);
+        current = root;
+    }
+
+    ~FileSystem(){
+        delete root; //recursively deletes entire tree
+    }
+
+    void mkdir(const std::string& path);
+    void touch(const std::string& path);
+    void ls(const std::string& path = "");
+    void cd (const std::string& path);
+    void pwd() const;
+    void rm(const std::string& path);
+    void cat(const std::string& path);
+    void write(const std::string& path,const std::string& data);
+
+private:
+    Node* resolvePath(const std::string& path);
+    std::vector<std::string> tokenize(const std::string& path) const;
+};
