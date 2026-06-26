1bca8d6ab4e006d403c84216ac80c567705c8db8
Giffin-MH-Osborne <giffin.mh.osborne@gmail.com>
2026-06-26 17:27:32 -0400

    Initial Commit

---
+++ Directory.h
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include "Node.h"
+
+class Directory : public Node {
+    std::vector<Node*> children; 
+
+public:
+    Directory(const std::string& name, Node* parent)
+        :  Node(name, parent) {}
+
+    ~Directory() override {
+        for(Node* child : children){
+            delete child;
+        }
+    }
+
+    NodeType getType() const override {return NodeType::Directory;}
+
+    void addChild(Node* child) {
+        children.push_back(child);
+    }
+
+    const std::vector<Node*>& getChildren() const {
+        return children;
+    }
+
+    Node* findChild(const std::string& name) const;
+    void removeChild(const std::string& name);
+};
---
+++ Node.h
@@ -0,0 +1,18 @@
+#include <string>
+
+enum class NodeType {File, Directory};
+
+class Node {
+protected:
+    std::string name;
+    Node* parent;
+public:
+    Node(const std::string& name, Node* parent)
+        : name(name), parent(parent) {}
+    
+    virtual ~Node() = default;
+
+    const std::string& getName() const {return name;}
+    Node* getParent() const {return parent;}
+    virtual NodeType getType() const = 0;
+};
